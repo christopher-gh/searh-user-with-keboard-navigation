299a91b232c29ee68a41fc60b3070fa90b3975ae
Christopher Sanchez <christophercsanchez@gmail.com>
2020-12-01 15:09:28 +0800

    Adde global styles and roboto fonts

---
+++ src/globalStyles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+import 'normalize.css';
+
+export const GlobalStyles = createGlobalStyle`
+    body{
+        font-family: 'Roboto', sans-serif;
+        font-size: 1rem;
+    }
+`
